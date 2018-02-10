0c4205fae11621c4933e6ea9a361ce369ff88af0
monysz2 <32449800+monysz2@users.noreply.github.com>
2018-02-10 18:27:15 +0100

    Final version

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,33 +1,23 @@
 package sample;
 
-import javafx.application.Application;
+
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 
-import java.awt.event.MouseEvent;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.Socket;
 import java.net.URL;
-import java.util.LinkedList;
-import java.util.ResourceBundle;
-import java.util.Scanner;
-import javafx.event.EventHandler;
-import javafx.stage.Stage;
+import java.util.*;
 
-public class Controller implements Initializable{
+public class Controller implements Initializable {
     BufferedReader in = null;
     PrintWriter out = null;
     @FXML
@@ -35,169 +25,298 @@ public class Controller implements Initializable{
     @FXML
     HBox player2Cards;
     @FXML
-    Button putCard;
-    @FXML
     Button getCard;
-    @FXML
-    Button start;
     @FXML
     ImageView set;
     @FXML
     ImageView table;
     @FXML
-    Label state = new Label();
+    Label state;
     @FXML
-            ImageView userFieldToPut;
-    int stat,strike=0;
-    String response = null;
-    String cardToPut = null;
+    ImageView userFieldToPut;
+    @FXML
+    Label color;
+    @FXML
+            Label cardsToTake;
+    @FXML
+            Label turnsToWait;
+    @FXML
+            Label value;
+
+    Button cardBuffer = new Button();
+
+
     LinkedList<Button> cards = new LinkedList<>();
-    Manager manager;
+    LinkedList<ImageView> players2cards = new LinkedList<>();
 
-    public Controller(BufferedReader reader, PrintWriter writer) throws IOException {
-        this.in = reader;
-        this.out = writer;
+    public Controller() throws IOException {
     }
-
-    public Controller() throws IOException{}
-
 
 
     @FXML
     void getCard() throws IOException {
+        out.println("0");
+        getCard.setDisable(true);
+
+    }
+
+    void makao(int player)
+    {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("MAKAO!");
+        alert.setHeaderText(null);
+        alert.setContentText("MAKAO Player"+player+"!!!");
+        alert.showAndWait();
+    }
+
+    void finish(int player)
+    {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Finish");
+        alert.setHeaderText(null);
+        alert.setContentText("Player"+player+"won !!!");
+        alert.showAndWait();
+        Platform.exit();
+    }
+
+
+    void giveValue()
+    {
+        List<String> choices = new ArrayList<>();
+        choices.add("5");
+        choices.add("6");
+        choices.add("7");
+        choices.add("8");
+        choices.add("9");
+        choices.add("10");
+        choices.add("none");
+
+        ChoiceDialog<String> dialog = new ChoiceDialog<>("5",choices);
+        dialog.setTitle("Give a value");
+        dialog.setHeaderText("Give a value");
+
+        Optional<String> result = dialog.showAndWait();
+        String choice = result.get();
+        if(choice.equals("none"))
+        {
+            out.println("11");
+        }else
+            out.println(choice);
+    }
+    void giveColor()
+    {
+        Alert alert = new Alert((Alert.AlertType.CONFIRMATION));
+        alert.setTitle("Give a Color");
+        alert.setHeaderText("Give a Color");
+
+        ButtonType hearts = new ButtonType("Hearts");
+        ButtonType diamonds = new ButtonType("Diamonds");
+        ButtonType spades = new ButtonType("Spades");
+        ButtonType clubs = new ButtonType("Clubs");
+
+
+        alert.getButtonTypes().setAll(hearts,diamonds,spades,clubs);
+
+        Optional<ButtonType> result = alert.showAndWait();
+        if(result.get() == hearts)
+        {
+            out.println("hearts");
+        }else if(result.get()==diamonds)
+        {
+            out.println("diamonds");
+        }else if(result.get() == spades)
+        {
+            out.println("spades");
+        }else if(result.get()==clubs)
+        {
+            out.println("clubs");
+        }
+    }
+
+    void displayCardToPut(String card) {
+        userFieldToPut.setImage(new Image("cards/" + card));
+    }
+
+    void displaySet() {
+
+        player1Cards.getChildren().addAll(cards);
+
+    }
+
+    void clearSet() {
+        player1Cards.getChildren().removeAll(cards);
+    }
+
+    void disableButtons() {
+        for (Button button : cards) {
+            button.setDisable(true);
+        }
+        getCard.setDisable(true);
+    }
+
+    void enableButtons() {
+        for (Button button : cards) {
+            button.setDisable(false);
+        }
+        getCard.setDisable(false);
+    }
+
+    void putCard(String card) throws IOException {
+
         out.println("1");
+        out.println(card);
+
+
+    }
+    void displayColor() throws IOException {
+        color.setText(in.readLine());
+    }
+
+    void displayValue() throws IOException {
+        int valueToDisplay = Integer.parseInt(in.readLine());
+        if(valueToDisplay==10)
+        {
+            value.setText("Jack");
+        }else
+            value.setText(Integer.toString(valueToDisplay));
+    }
+
+    void displayCardsToTake() throws IOException {
+        cardsToTake.setText(in.readLine());
+    }
+    void displayTurnsToWait() throws IOException {
+        turnsToWait.setText(in.readLine());
+    }
+    @FXML
+    void receiveCard() throws IOException {
+        System.out.println("Recieved");
         Button toAdd = new Button();
-        String card = in.readLine();
-        ImageView image = new ImageView("cards/"+card);
+        String card = new String(in.readLine());
+        ImageView image = new ImageView("cards/" + card);
         image.setFitHeight(100);
         image.setFitWidth(80);
         toAdd.setGraphic(image);
         toAdd.setOnMouseClicked(new EventHandler<javafx.scene.input.MouseEvent>() {
             @Override
             public void handle(javafx.scene.input.MouseEvent event) {
+                disableButtons();
                 displayCardToPut(card);
+                clearSet();
+                cardBuffer = toAdd;
+                cards.remove(toAdd);
+                displaySet();
+                try {
+                    putCard(card);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         });
+        clearSet();
         cards.add(toAdd);
         displaySet();
-        getCard.setDisable(true);
-
     }
-
-
-
-    void displayCardToPut(String card)
+    void displayPlayer2Cards(int number)
     {
-        userFieldToPut.setImage(new Image("cards/"+card));
-    }
-    void displaySet()
-    {
-
-            player1Cards.getChildren().addAll(cards);
-
-    }
-    void clearSet()
-    {
-            player1Cards.getChildren().removeAll(cards);
-    }
-    void disableButtons()
-    {
-        for(Button button : cards)
+        player2Cards.getChildren().removeAll(players2cards);
+        players2cards.clear();
+        for(int i=0;i<number;i++)
         {
-            button.setDisable(true);
-        }
-    }
-
-    void enableButtons()
-    {
-        for(Button button : cards)
-        {
-            button.setDisable(false);
-        }
-    }
-void addCardToSet(String card)
-{
-    Button toAdd = new Button();
-    ImageView image = new ImageView("cards/"+card);
-    image.setFitHeight(100);
-    image.setFitWidth(80);
-    toAdd.setGraphic(image);
-    toAdd.setOnMouseClicked(new EventHandler<javafx.scene.input.MouseEvent>() {
-        @Override
-        public void handle(javafx.scene.input.MouseEvent event) {
-            putCard(image.getImage().getUrl());
-            clearSet();
-            cards.remove(toAdd);
-            displaySet();
-        }
-    });
-    cards.add(toAdd);
-}
-    void putCard(String card)
-    {
-        String[] abc = card.split("/");
-        cardToPut = abc[10];
-
-    }
-@FXML
-    void startGame() throws IOException {
-        //stat = Integer.parseInt(in.next());
-        //out.println("3");
-        //state.setText("Waiting for player 2");
-
-        //String aaa = in.next();
-
-
-        for(int i=0; i<5; i++)
-        {
-            //1 = ready
-            out.println("1");
-            Button toAdd = new Button();
-            String card=new String(in.readLine());
-            ImageView image = new ImageView("cards/"+card);
+            ImageView image = new ImageView("cards/opponent.png");
             image.setFitHeight(100);
             image.setFitWidth(80);
-            toAdd.setGraphic(image);
-            toAdd.setOnMouseClicked(new EventHandler<javafx.scene.input.MouseEvent>() {
-                @Override
-                public void handle(javafx.scene.input.MouseEvent event) {
-                    displayCardToPut(card);
+            players2cards.add(image);
+        }
+        player2Cards.getChildren().addAll(players2cards);
+    }
+    @FXML
+    void waitForServer() throws IOException {
+        if (in.ready()) {
+            String choice = in.readLine();
+            System.out.println("Server>>>"+ choice);
+            switch (Integer.parseInt(choice)) {
+                case 10:
+                    receiveCard();
+                    break;
+                case -10:
+                    disableButtons();
+                case -20:
+                    state.setText("Wrong choice - try again");
                     clearSet();
-                    cards.remove(toAdd);
+                    cards.add(cardBuffer);
                     displaySet();
-                }
-            });
-            cards.add(toAdd);
+                    enableButtons();
+                    break;
+                case 20:
+                    enableButtons();
+                    state.setText("Your turn - put card");
+                    break;
+                case 30:
+                    giveColor();
+                    break;
+                case 40:
+                    giveValue();
+                    break;
+                case 5:
+                    displayColor();
+                    break;
 
+                case 6:
+                    displayCardsToTake();
+                    break;
+                case 7:
+                    displayTurnsToWait();
+                    break;
+                case 8:
+                    displayValue();
+                case 4:
+                    table.setImage(new Image("cards/" + in.readLine()));
+                    break;
+                case 2:
+                    state.setText("Player's 2 move");
+                    disableButtons();
 
+                    break;
+                case 1:
+                    state.setText("Player's 1 move");
+                    enableButtons();
 
+                    break;
+                case 9:
+                    displayPlayer2Cards(Integer.parseInt(in.readLine()));
+                    break;
+                case 100:
+                    makao(1);
+                    break;
 
+                case 101:
+                    makao(2);
+                    break;
 
+                case 200:
+                    finish(1);
+                    break;
 
+                case 201:
+                    finish(2);
+                    break;
+
+            }
         }
-    displaySet();
-        if(stat==1)
-        {
-
-           disableButtons();
-
-
-        }
-
-
 
     }
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        Platform.setImplicitExit(false);
         try {
-            manager = new Manager(Main.socket);
+            in = new BufferedReader(new InputStreamReader(Main.socket.getInputStream()));
+            out = new PrintWriter(Main.socket.getOutputStream(), true);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        manager.controller = this;
-        manager.start();
+
 
     }
 }
+
